d1520a78f04dcb9910ac29d8afffeaef71f5914d
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-01-28 10:44:36 -0300

    Criar enum_meses.ts

---
+++ Tipos/enum_meses.ts
@@ -0,0 +1,18 @@
+enum Direcoes{
+    Janeiro = 1,
+    Fevereiro,
+    Marco,
+    Abril,
+    Maio,
+    Junho,
+    Julho,
+    Agosto,
+    Setembro,
+    Outubro,
+    Novembro,
+    Dezembro
+}
+
+
+console.log(Direcoes.Fevereiro)
+console.log(Direcoes[12])
